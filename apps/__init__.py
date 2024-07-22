e20d71bd3fa95a63ad5be60ae90671efaf2b01ab
luoxin <1260813538@qq.com>
2024-07-22 23:46:40 +0800

    修改加载配置文件注释

--- apps/__init__.py
+++ apps/__init__.py
@@ -12,7 +12,7 @@ from apps.views.house import house_bp
 def create_app():
     # 在创建app对象的时候，如果需要找模版目录会默认在自己的层级去找，因为templates和当前的app对象不在一个层级，所以需要重新定义
     app = Flask(__name__,template_folder='/Users/luoxin/workspace/practice_blueprint/templates')
-    # 加在配置
+    # 加载配置。通过导入settings文件中不同的类来控制环境变量
     app.config.from_object(DevelopmentConfig)
     # 注册蓝图，将不同模块中创建的蓝图对象注册到app上，这样app在启动的时候，就能和不同模块中的视图函数进行关联
     app.register_blueprint(index_bp)
